d55326da594d2c690234a0d8aee8f56de0de7ba6
Jeremy Johns <jez.johns@gmail.com>
2023-12-13 20:30:48 +0000

    forgot

--- spares.js
+++ spares.js
@@ -2,4 +2,6 @@ export function impOne(moi) {
 	const h1 = document.querySelector("h1");
 	const para = document.createElement("p");
 	para.textContent = "Help me, " + moi + "! You're my only hope";
+	h1.appendChild(para)
 }
+
